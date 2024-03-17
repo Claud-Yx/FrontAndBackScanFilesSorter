e6eb14937a06566216a2db6552c5ffe3a203536a
Cloudyx <durjs96@gmail.com>
2024-03-17 16:01:43 +0900

    Update FSGUI_PathGetter

--- GUI/FSGUI_Main.py
+++ GUI/FSGUI_Main.py
@@ -1,10 +1,29 @@
 from FSGUI import *
+import FSGUI_PathGetter as pg
+
+
+class MainGUI(tk.Tk):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+        self.title("Front And Back Scan Files Sorter")
+        self.geometry("640x400+100+100")
+        self.resizable(False, False)
+
+        # File Paths
+        self.path_label_frame = tk.LabelFrame(self, text="Paths")
+        self.path_label_frame.pack()
+        self.pg_source = pg.PathGetterFrame(self.path_label_frame)
+        self.pg_destination = pg.PathGetterFrame(self.path_label_frame)
+
+        self.pg_source.SetFrameName("Source Path")
+        self.pg_destination.SetFrameName("Destination Path")
+
+        self.pg_source.pack(side="top", anchor='e')
+        self.pg_destination.pack(side="top", anchor='e')
+
 
 if __name__ == '__main__':
-    window = Tk()
-
-    window.title("Test Window")
-    window.geometry("640x400+100+100")
-    window.resizable(False, False)
+    window = MainGUI()
 
     window.mainloop()
--- GUI/FSGUI_PathGetter.py
+++ GUI/FSGUI_PathGetter.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.filedialog
 
 from FSGUI import *
 
@@ -7,38 +8,71 @@ class PathGetterFrame(Frame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
 
-        self.path = tk.StringVar(self, value="Path Here", name="path")
+        # Frame Name Label
+        self.frame_name_label = tk.Label(self)
+        self.frame_name = tk.StringVar(
+            master=self,
+            value="Frame Name"
+        )
+        self.frame_name_label.configure(
+            textvariable=self.frame_name
+        )
+        self.frame_name_label.pack(side="left")
 
+        # Path Label(Text)
+        self.path = tk.StringVar(self, value="Path Here", name="path")
         self.path_label = tk.Text(self)
         self.path_label.configure(
-            relief="solid",
+            state="disabled",
+            relief="groove",
             pady=2,
             padx=2,
-            width=30,
+            wrap="none",
+            width=40,
             height=0
         )
-        self.path_label.insert(0.0, self.path.get())
+        self._ChangePath("Path Here")
+        self.path_label.pack(side="left")
 
-        #         (
-        #     textvariable=self.path,
-        #     relief="solid",
-        #     borderwidth=2,
-        #     padding=2,
-        # )
-        self.path_label.pack(side="left", anchor="w")
+        # Path Getter Button(Button)
+        self.path_getter_button = Button(
+            self,
+            text="...",
+            width=2,
+            command=self._GetPath_ButtonFunc)
+        self.path_getter_button.pack(side="left")
 
-        self.path_getter_button = Button(self)
-        self.path_getter_button.pack(side="left", anchor="e")
+    def _ChangePath(self, text):
+        self.path.set(text)
+        self.path_label.configure(state="normal")
+        self.path_label.delete(1.0, "end")
+        self.path_label.insert(1.0, self.path.get())
+        self.path_label.configure(state="disabled")
+
+    def _GetPath_ButtonFunc(self):
+        path = tkinter.filedialog.askdirectory(
+            parent=self.path_getter_button,
+            initialdir='./'
+        )
+        self._ChangePath(path)
+
+    def SetFrameName(self, text):
+        self.frame_name.set(text)
+
+    def GetPath(self):
+        return self.path.get()
 
 
 if __name__ == '__main__':
     window = tk.Tk()
     window.geometry("600x300")
 
-    pg_frame = PathGetterFrame(window)
-    pg_frame.pack()
+    pg_frame1 = PathGetterFrame(window)
+    pg_frame2 = PathGetterFrame(window)
+    pg_frame1.pack()
+    pg_frame2.pack()
+
+    pg_frame1.SetFrameName("Hello")
+    pg_frame2.SetFrameName("Hi")
 
     window.mainloop()
-
-
-
